f18feae774f6e3f23dd011a3ec74eacde151f9fa
lnuvy <hangeulgod@naver.com>
2022-01-06 17:13:01 +0900

    DOM 삽입전 검색 체크

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-const img = document.getElementById('photo')
-console.log(img);
+const div = document.createElement('div')
+div.className = "parent"
+div.innerText = "Hello World !"
+
+const img = document.createElement('img')
+img.id = "photo"
+img.src = "index.js"
+img.alt = "photo"
+
+const p = document.createElement('p')
+p.className = "summary"
+p.innerText = "this is summary !"
+
+div.append(img, p)
+
+
+const photo = document.getElementById('photo')
+console.log(photo);
+
+const root = document.getElementById('root')
+root.appendChild(div)
+
+const photo2 = document.getElementById('photo')
+console.log(photo2);
