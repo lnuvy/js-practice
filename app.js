e17d5fce76ae67b0562273a2af5cc8122ba124eb
lnuvy <hangeulgod@naver.com>
2022-01-06 17:08:57 +0900

    DOM조회(검색)

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-
-const div = document.createElement('div')
-div.className = "parent"
-
-
-// div.innerText = "Hello~"
-// const img = document.createElement('img')
-// img.className = "child"
-// img.src = "http://127.0.0.1:5500"
-// img.alt = "photo"
-//
-// const p = document.createElement('p')
-// p.className = "summary"
-// p.innerText = "this is summary !"
-
-// div.append(img, p)
-
-
-div.innerHTML = `
-  "Hello~"
-  <img class="child" src="http://127.0.0.1:5500" alt="photo" />
-  <p class="summary">this is summary !</p>
-`
-
-const root = document.getElementById('root')
-root.appendChild(div)
+const img = document.getElementById('photo')
+console.log(img);
