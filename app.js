64f87043ca9eb8731adbae8a4beef6168d562750
lnuvy <hangeulgod@naver.com>
2022-01-06 17:23:45 +0900

    DOM 클래스이름 검색 ElementsByClassName

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 
-// 클래스명이 photo-item 인 요소 검색
-const photos = document.querySelectorAll('.photo-item')
-console.log(photos)
+const photos = document.getElementsByClassName('photo-item')
 
-// 클래스명이 photo-item 이고, 하위요소가 img 태그인 요소 검색
-const imgs = document.querySelectorAll('.photo-item img')
-console.log(imgs)
+console.log(photos);
 
-console.log(imgs[0].src);
-
-for (let img of imgs) {
-  console.log(img);
+for (let photo of photos) {
+  console.log(photo);
 }
