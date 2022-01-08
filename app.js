5ddd2f7545446f6d8b09142fc7c87e54fa82f521
lnuvy <hangeulgod@naver.com>
2022-01-08 14:26:38 +0900

    callback 함수와 이벤트핸들러 외부로 분리

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const popup = document.getElementById('popup')
+// const popupBtn = document.getElementById('popup')
+// popupBtn.addEventListener('click', () => {
+//   alert('팝업')
+// })
 
-popup.onclick = function () {
-  alert('팝업')
+function popup() {
+  alert('팝업창')
 }
+
+const popupBtn = document.getElementById('popup')
+popupBtn.addEventListener('click', popup)
