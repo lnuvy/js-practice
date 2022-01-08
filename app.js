339c2cf2c0ed82fe3344dc3aeae193c656b8502b
lnuvy <hangeulgod@naver.com>
2022-01-08 16:52:23 +0900

    classList 프로퍼티 사용하기

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
-// function popup(e) {
-//   e.target.style.all = 'unset'
-//   e.target.style.position = 'absolute'
-//   e.target.style.left = '500px'
-//   e.target.style.width = '200px'
-//   e.target.style.height = '70px'
-//   e.target.style.textAlign = 'center'
-//   e.target.style.background = 'skyblue'
-//   e.target.style.borderRadius = '50%'
-//   e.target.style.transition = 'all ease 2.5s'
-// }
-
-// const popupBtn = document.getElementById('popup')
-// popupBtn.addEventListener('click', popup)
-
-
 function popup(e) {
   const target = e.target
-  const targetStyle = target.style
-
-  targetStyle.all = 'unset'
-  targetStyle.position = 'absolute'
-  targetStyle.left = '500px'
-  targetStyle.width = '200px'
-  targetStyle.height = '70px'
-  targetStyle.textAlign = 'center'
-  targetStyle.background = 'skyblue'
-  targetStyle.borderRadius = '50%'
-  targetStyle.transition = 'all ease 2.5s'
+  target.classList.add('circle')
 }
 
-const popupBtn = document.getElementById('popup')
+const popupBtn = document.querySelector('.popup')
 popupBtn.addEventListener('click', popup)
