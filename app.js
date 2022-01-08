8f91fe64f55976223aad1ec378ed7327b8d435f7
lnuvy <hangeulgod@naver.com>
2022-01-08 16:46:16 +0900

    이벤트핸들러 함수 해제

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 function popup() {
   alert('팝업창')
-}
-function setBtnColor() {
-  popupBtn.style.background = 'skyblue'
-}
-function unsetBtnColor() {
-  popupBtn.style.background = ''
+  popupBtn.removeEventListener('click', popup) // 1회 이후 이벤트를 제거
 }
 
 const popupBtn = document.getElementById('popup')
 popupBtn.addEventListener('click', popup)
-popupBtn.addEventListener('mouseover', setBtnColor)
-popupBtn.addEventListener('mouseout', unsetBtnColor)
