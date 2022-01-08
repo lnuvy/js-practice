15fdfb27192391f41fa06ce7dcdc91b7e6f29e5f
lnuvy <hangeulgod@naver.com>
2022-01-08 14:27:49 +0900

    하나의 요소에 여러가지 이벤트핸들러 등록하기

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// const popupBtn = document.getElementById('popup')
-// popupBtn.addEventListener('click', () => {
-//   alert('팝업')
-// })
-
 function popup() {
   alert('팝업창')
+}
+function setBtnColor() {
+  popupBtn.style.background = 'skyblue'
+}
+function unsetBtnColor() {
+  popupBtn.style.background = ''
 }
 
 const popupBtn = document.getElementById('popup')
 popupBtn.addEventListener('click', popup)
+popupBtn.addEventListener('mouseover', setBtnColor)
+popupBtn.addEventListener('mouseout', unsetBtnColor)
