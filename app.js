09de3b99bb8ff56b49b7aa9f05ed4dcf8b021765
lnuvy <hangeulgod@naver.com>
2022-01-08 16:49:48 +0900

    target 스타일 직접변경

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-function popup(e) {  // 변수이름은 주로 e, evt, event 로 사용
-  console.log(e);
-  console.log(e.target);
+// function popup(e) {
+//   e.target.style.all = 'unset'
+//   e.target.style.position = 'absolute'
+//   e.target.style.left = '500px'
+//   e.target.style.width = '200px'
+//   e.target.style.height = '70px'
+//   e.target.style.textAlign = 'center'
+//   e.target.style.background = 'skyblue'
+//   e.target.style.borderRadius = '50%'
+//   e.target.style.transition = 'all ease 2.5s'
+// }
+
+// const popupBtn = document.getElementById('popup')
+// popupBtn.addEventListener('click', popup)
+
+
+function popup(e) {
+  const target = e.target
+  const targetStyle = target.style
+
+  targetStyle.all = 'unset'
+  targetStyle.position = 'absolute'
+  targetStyle.left = '500px'
+  targetStyle.width = '200px'
+  targetStyle.height = '70px'
+  targetStyle.textAlign = 'center'
+  targetStyle.background = 'skyblue'
+  targetStyle.borderRadius = '50%'
+  targetStyle.transition = 'all ease 2.5s'
 }
 
 const popupBtn = document.getElementById('popup')
